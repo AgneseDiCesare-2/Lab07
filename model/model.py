d69e20e52eeb7c12c31133746962b741227404e4
agnese <s325169@studenti.polito.it>
2026-04-23 15:26:42 +0200

    ora stampa (ma sbagliato). a win is a win

--- model/model.py
+++ model/model.py
@@ -7,44 +7,44 @@ class Model:
         self._soluzione={} #costo: soluzione
                             #float: list()
         self._x=None #valore booleano che dice se nel costo devo considerare 100 oppure no
-        self._umidita=0
 
     def get_avg_umidita(self, mese):
         from database.meteo_dao import MeteoDao
         avg=MeteoDao().get_avg_umidita(mese) #dizionario
         return avg
 
-    def leggi_umidita(self, citta, giorno):
+    def leggi_umidita(self, citta, giorno) -> float: #restituisce il costo parziale
         from database.meteo_dao import MeteoDao
-        umidita = MeteoDao().get_umidita_giorno(citta, giorno)
+        umidita = MeteoDao().get_umidita_giorno(citta, giorno) #restituisce un valore numerico
         if umidita is None:
-            return False
-        self._umidita = umidita
-        return True
+             return False
+        costo=self.calcola_costo_giorno(umidita, self._x)
+        return costo
 
     def calcola_sequenza(self, mese):
         self._soluzione = {}
         self._ricorsione([], ["Milano", "Torino", "Genova"], mese, 0)
         if not self._soluzione:
             return None, []
-        prima_chiave = max(self._soluzione.keys())
+        prima_chiave = min(self._soluzione.keys())
         primo_valore = self._soluzione[prima_chiave]
         return prima_chiave, primo_valore
 
     def controlla_tregiorni(self, parziale: list, i: str):
-        l=len(parziale)
-        if l<=3:
-            self._x=1
+        l = len(parziale)
+
+        if l < 3:
+            self._x = 1
             return True
 
-        elif parziale[-1]==i: #non sto cambiando città
-            self._x=0 #non considero 100$
+        elif parziale[-1] == i:  # resto nella stessa città
+            self._x = 0
             return True
-        #sto cambiando città --> devo controllare che quella di prima compaia tra volte di seguito
-        else:
+
+        else:  # cambio città: la precedente deve essere stata fatta per 3 giorni consecutivi
             ultimo = parziale[-1]
-            if parziale[-2]==ultimo and parziale[-3]==i:
-                self._x = 1  # considero i 100$
+            if parziale[-2] == ultimo and parziale[-3] == ultimo:
+                self._x = 1
                 return True
             return False
 
@@ -64,20 +64,19 @@ class Model:
 
     #parziale è la soluzione parziale, resto sono le città che non sono ancora state analizzate dal tecnico
     def _ricorsione(self, parziale, resto, mese, costo_parziale):
-        costo_soluzione=0
         if (len(parziale) == 15): #tutte le città sono state analizzate
-            self._soluzione[costo_soluzione]=parziale #dizionario
+            self._soluzione[costo_parziale] = copy.deepcopy(parziale)
             return
 
         else:
-            for i in (resto):
+            for i in (resto): #i è la città
                 if self.controlla_tregiorni(parziale, i) and self.controlla_seigiorni(parziale, i):
                     #imposta self._x
                     parziale.append(i)
                     giorno = date(2013, mese, len(parziale))
-                    self.leggi_umidita(i, giorno)
-                    costo_giornaliero = self.calcola_costo_giorno(self._umidita, self._x)
-                    self._ricorsione(parziale, resto, mese, costo_parziale + costo_giornaliero)
+                    costo_giornaliero=self.leggi_umidita(i, giorno)
+                    #costo_giornaliero = self.calcola_costo_giorno(self._umidita, self._x)
+                    self._ricorsione(parziale, resto, mese, costo_parziale+ costo_giornaliero)
 
                     parziale.pop()
 
